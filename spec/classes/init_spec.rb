a58605416416480228d511827f062a38b8bafeae
Christian González <christiangda@gmail.com>
2016-01-07 21:22:58 +0000

    improved: reparing testing

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,33 @@
+# spec/classes/init_spec.rb
 require 'spec_helper'
-describe 'powerdns' do
 
-  context 'with defaults for all parameters' do
-    it { should contain_class('powerdns') }
+describe 'Class powerdns', :type => 'class' do
+
+  context 'On a Debian OS with defaults for all parameters' do
+     let :facts do
+      {
+        :osfamily => 'Debian'
+      }
+    end
+    it {
+      should contain_class('powerdns')
+      should contain_package('pdns-server')
+      should contain_service('pdns')
+    }
   end
+
+  context 'On a RedHat OS with defaults for all parameters' do
+     let :facts do
+      {
+        :osfamily => 'RedHat'
+      }
+    end
+    it {
+      should contain_class('powerdns')
+      should contain_package('pdns')
+      should contain_package('pdns-tools')
+      should contain_service('pdns')
+    }
+  end
+
 end
