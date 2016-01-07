7402270590d240800269361d710c43e66b54a4a1
Christian González <christiangda@gmail.com>
2016-01-07 23:48:46 +0000

    improved: first functional testing to class init.pp

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,33 +1,41 @@
-# spec/classes/init_spec.rb
-require 'spec_helper'
+describe 'powerdns' do
+  shared_examples 'a Linux distribution' do |osfamily|
 
-describe 'Class powerdns', :type => 'class' do
+    context "with default parameters on #{osfamily}" do
+      let (:facts) do
+        {
+          :operatingsystem => osfamily,
+          :osfamily        => osfamily,
+        }
+      end
 
-  context 'On a Debian OS with defaults for all parameters' do
-     let :facts do
-      {
-        :osfamily => 'Debian'
-      }
+      it { should compile.with_all_deps }
+      it { should create_class('powerdns') }
+      it { should contain_class('powerdns::params') }
+
+      if osfamily == 'Debian'
+        it { should create_file('/etc/powerdns/pdns.conf') }
+        it { should contain_package('pdns-server') }
+        it { should contain_powerdns__config('pdns.conf') }
+        it { should contain_powerdns__install('pdns-server') }
+        it { should contain_powerdns__service('pdns') }
+        it { should contain_service('pdns') }
+      end
+      if osfamily == 'RedHat'
+        it { should create_file('/etc/pdns/pdns.conf') }
+        it { should contain_package('pdns') }
+        it { should contain_package('pdns-tools') }
+        it { should contain_powerdns__config('pdns.conf') }
+        it { should contain_powerdns__install('pdns') }
+        it { should contain_powerdns__install('pdns-tools') }
+        it { should contain_powerdns__service('pdns') }
+        it { should contain_service('pdns') }
+        it { should contain_powerdns__config('pdns.conf') }
+      end
+
     end
-    it {
-      should contain_class('powerdns')
-      should contain_package('pdns-server')
-      should contain_service('pdns')
-    }
   end
 
-  context 'On a RedHat OS with defaults for all parameters' do
-     let :facts do
-      {
-        :osfamily => 'RedHat'
-      }
-    end
-    it {
-      should contain_class('powerdns')
-      should contain_package('pdns')
-      should contain_package('pdns-tools')
-      should contain_service('pdns')
-    }
-  end
-
+  it_behaves_like 'a Linux distribution', 'RedHat'
+  it_behaves_like 'a Linux distribution', 'Debian'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,16 @@
-require 'rspec-puppet/spec_helper'
+require 'puppetlabs_spec_helper/module_spec_helper'
+
+fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
+
+RSpec.configure do |c|
+
+  c.module_path = File.join(fixture_path, 'modules')
+  c.manifest_dir = File.join(fixture_path, 'manifests')
+
+  c.default_facts = {
+    :kernel          => 'Linux',
+    :puppetversion   => ENV['PUPPET_VERSION'] || Puppet.version
+  }
+end
+
+at_exit { RSpec::Puppet::Coverage.report! }
