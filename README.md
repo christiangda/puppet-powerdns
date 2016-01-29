9284512117e70707aff8a6b2a0922ef0a3bff8f0
Christian González <christiangda@gmail.com>
2016-01-29 13:40:03 +0000

    improved: improved docs

--- README.md
+++ README.md
@@ -155,18 +155,15 @@ class { '::powerdns::recursor':
 
 ## Reference
 
-**under construction**
-
-Here, list the classes, types, providers, facts, etc contained in your module.
-This section should include all of the under-the-hood workings of your module so
-people know what the module is touching on their system but don't need to mess
-with things. (We are working on automating this section!)
+[PowerDNS](https://www.powerdns.com/)
+* https://doc.powerdns.com
+* https://doc.powerdns.com/md/authoritative/settings/
+* https://doc.powerdns.com/md/authoritative/
+* https://doc.powerdns.com/md/recursor/settings/
 
 ## Limitations
 
-**under construction**
-
-This is where you list OS compatibility, version compatibility, etc.
+This module could not manage DNS records, this only can be used as configuration of [PowerDNS](https://www.powerdns.com/).
 
 ## Development
 
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,32 +1,5 @@
 # == Class: powerdns
 #
-# Full description of class powerdns here.
-#
-# === Parameters
-#
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the function of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
-#
-# === Examples
-#
-#  class { 'powerdns':
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#  }
-#
 # === Authors
 #
 # Christian Gonzalez <christiangda@gmail.com>
