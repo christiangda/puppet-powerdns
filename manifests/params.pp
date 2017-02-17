00fe2eb45dba770a54ef91247218fd6ae9473471
Arthur Barton (arthurb) <arthurb@igniferous.net>
2017-02-17 14:50:16 +1100

    enable rping service control command for xenial

--- manifests/params.pp
+++ manifests/params.pp
@@ -63,11 +63,6 @@ class powerdns::params {
       $recursor_user             ='pdns'
       $recursor_group            ='pdns'
       $backend_file_perms        = '0640'
-      case $::lsbdistcodename {
-        'xenial': {
-          $service_status_cmd = '/usr/bin/pdns_control rping 2>/dev/null 1>/dev/null'
-        }
-      }
     }
     default: {
       fail("\"${module_name}\" provides no package default value
@@ -81,7 +76,13 @@ class powerdns::params {
   $service_manage     = true
   $service_restart    = true
   $service_status     = true
-  $service_status_cmd = '/usr/bin/pdns_control ping 2>/dev/null 1>/dev/null'
+  case $::lsbdistcodename {
+    'xenial': {
+      $service_status_cmd = '/usr/bin/pdns_control rping 2>/dev/null 1>/dev/null'
+    }
+      default: {
+    $service_status_cmd = '/usr/bin/pdns_control ping 2>/dev/null 1>/dev/null'
+  }
   $config_include_dir = "${config_file_path}/pdns.d"
   $config_file_backup = true
   $default_config     = {
